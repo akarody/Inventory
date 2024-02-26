604d66ceb5e54efeb07c445947f30aa2a1fe9422
Ankit Karody <ankit.karody@gmail.com>
2024-02-27 01:11:51 +0530

    component rerenders when csv file is uploaded

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,37 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import "./Home.css"
-import Navbar from '../components/Navbar'
-import DataTable from '../components/Datatable'
-import axios from "axios"
+import React, { useEffect, useState } from 'react';
+import "./Home.css";
+import Navbar from '../components/Navbar';
+import DataTable from '../components/Datatable';
+import axios from "axios";
+
 const Home = () => {
     const [cars, setCars] = useState([]);
 
-
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:5000/view/cars');
-                setCars(response.data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
         fetchData();
     }, []);
 
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/view/cars');
+            setCars(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
+    const handleFileUpload = () => {
+        // Trigger re-render by fetching data again
+        fetchData();
+    };
 
     return (
-
         <div className='Home'>
-            <Navbar />
-
+            <Navbar onFileUpload={handleFileUpload} />
             <DataTable data={cars} />
-
-
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
