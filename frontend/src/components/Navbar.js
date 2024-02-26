dc18c8234868f07d4c3632e1106ada989d6f5028
Ankit Karody <ankit.karody@gmail.com>
2024-02-27 00:07:39 +0530

    added import csv

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const Navbar = () => {
+
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    const handleSubmit = async () => {
+        if (!file) {
+            console.error('No file selected');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('csvFile', file);
+        console.log(formData)
+
+        try {
+            const response = await fetch('http://localhost:5000/csv/upload-csv', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (response.ok) {
+                console.log('File uploaded successfully');
+            } else {
+                console.error('Failed to upload file');
+            }
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
+    };
+
     return (
-        <div className='Navbar'>Inventory</div>
-    )
+        <div className="Navbar">
+            <input type="file" id="csvFileInput" accept=".csv" onChange={handleFileChange} style={{ display: 'none' }} />
+            <label htmlFor="csvFileInput">
+                <Button variant="outlined" component="span" onClick={handleSubmit}>Import CSV</Button>
+            </label>
+        </div>
+    );
 }
+
 
 export default Navbar
